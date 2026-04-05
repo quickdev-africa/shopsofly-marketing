85f3c82b39c5786ec698b75139a0faa56ccf5a1c
WebMastser <info.laserstarglobal@.com>
2026-04-05 01:19:49 +0100

    fix: remove default favicon.ico, use SVG favicon with correct title

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export const metadata: Metadata = {
     type: "website",
   },
   icons: {
-    icon: "/favicon.svg",
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
     shortcut: "/favicon.svg",
     apple: "/favicon.svg",
   },
